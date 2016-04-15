3af8e37ceb5d396f32f58192046bdccb8e6abf55
Matthew Sumner <matt.m.sumner@gmail.com>
2016-04-15 10:25:19 -0400

    Better WordList type

--- src/Main.hs
+++ src/Main.hs
@@ -7,12 +7,14 @@ import Data.List (intersperse)
 import System.Exit (exitSuccess)
 import System.Random (randomRIO)
 
-type WordList = [String]
+newtype WordList =
+  WordList [String]
+  deriving (Eq, Show)
 
 allWords :: IO WordList
 allWords = do
   dict <- readFile "data/dict.txt"
-  return (lines dict)
+  return $ WordList (lines dict)
 
 minWordLength :: Int
 minWordLength = 5
@@ -22,14 +24,14 @@ maxWordLength = 9
 
 gameWords :: IO WordList
 gameWords = do
-  aw <- allWords
-  return (filter gameLength aw)
+  (WordList aw) <- allWords
+  return $ WordList (filter gameLength aw)
   where gameLength w =
           let l = length (w :: String)
           in l > minWordLength && l < maxWordLength
 
 randomWord :: WordList -> IO String
-randomWord wl = do
+randomWord (WordList wl) = do
   randomIndex <- randomRIO (0, (length wl - 1))
   return $ wl !! randomIndex
 
